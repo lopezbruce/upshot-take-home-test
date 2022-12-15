75c7fcb685ff9a2a6ad6cbd954b863a9620ab350
Bruce Lopez <lopez.bruce@gmail.com>
2022-12-15 16:52:49 -0500

    Postgres Docker Compose up and running

---
+++ app/sql/create_tables.sql
@@ -0,0 +1,25 @@
+-- Creation of dim teams
+CREATE TABLE IF NOT EXISTS dim_teams (
+    id integer NOT NULL,
+    name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creation of dim competitions
+CREATE TABLE IF NOT EXISTS dim_competitions (
+    id integer NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creation of fact competitions
+CREATE TABLE IF NOT EXISTS fact_competitions (
+    competition_id integer NOT NULL,
+    team_id integer NOT NULL,
+        CONSTRAINT fk_team
+        FOREIGN KEY(team_id)
+	    REFERENCES dim_teams(id),
+        CONSTRAINT fk_competition
+        FOREIGN KEY(competition_id)
+	    REFERENCES dim_competitions(id)
+);
